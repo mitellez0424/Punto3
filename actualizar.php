f515fc579322865d9cf76420c3d2ee8c657e79dc
mitellez0424 <42224069+mitellez0424@users.noreply.github.com>
2023-03-03 15:57:44 -0500

    d

--- actualizar.php
+++ actualizar.php
@@ -1,15 +1,9 @@
 <?php 
     include("conexion.php");
-    
-
 $id=$_GET['id'];
-
 $sql="SELECT * FROM producto WHERE SKU='$id'";
 $query=mysqli_query($con,$sql);
-
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -18,8 +12,7 @@ $query=mysqli_query($con,$sql);
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
         <title>Actualizar</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">      
     </head>
     <body>
                 <div class="container mt-5">
@@ -34,12 +27,8 @@ $query=mysqli_query($con,$sql);
                                     <input type="tienda" class="form-control mb-3" name="Tienda" placeholder="Tienda" value="<?php echo $row['tienda']?>">
                                     <input type="file" class="form-control mb-3" name="Imagen" placeholder="URLimagen" value="<?php echo $row['Imagen']?>">
                                     <?php } ?>
-                                    
                             <input type="submit" class="btn btn-primary btn-block" value="Actualizar">
-                            
                     </form>
-
-                
                 </div>
     </body>
 </html>
